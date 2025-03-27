19c4bd05eb60a6b9e3c93aeca4573a4127df6919
Alisher Issayev <aliisherka@mail.ru>
2025-03-27 20:48:38 +0100

    feat: add houses search

--- src/controllers/houseController.ts
+++ src/controllers/houseController.ts
@@ -1,4 +1,5 @@
 import { Request, Response, NextFunction } from 'express';
+import { transliterate } from 'transliteration';
 import House from "../models/house";
 import HttpError from '../utils/HttpError';
 
@@ -27,17 +28,39 @@ export const getHouseById = async (req: Request, res: Response, next: NextFuncti
 
 export const createHouse = async (req: Request, res: Response, next: NextFunction) => {
   try {
-    const { title, country, price, rating } = req.body;
+    const { title, country, price, rating, city } = req.body;
     const imageUrls = (req.files as Express.Multer.File[])?.map(file => file.path);
 
-    if (!title || !price || !rating || !imageUrls) {
+    if (!title || !price || !rating || !imageUrls || !city) {
       return next(new HttpError('All fields are required', 400));
     }
 
-    const newHouse = new House({ title, country, price, rating, images: imageUrls });
+    const newHouse = new House({ title, country, price, rating, images: imageUrls, city });
 
     await newHouse.save();
     res.status(201).json(newHouse);
+  } catch (err) {
+    next(err);
+  }
+};
+
+export const searchHouses = async (req: Request, res: Response, next: NextFunction) => {
+  try {
+    const { location } = req.query;
+    if (!location || typeof location !== 'string') {
+      return next(new HttpError('Location parameter is required and must be a string', 400));
+    }
+
+    const searchLocation = transliterate(location.trim().toLowerCase());
+
+    const houses = await House.find({
+      $or: [
+        { city: { $regex: new RegExp(searchLocation, "i") } },
+        { country: { $regex: new RegExp(searchLocation, "i") } },
+      ],
+    });
+
+    res.status(200).json(houses);
   } catch (err) {
     next(err);
   }
--- src/routes/house.ts
+++ src/routes/house.ts
@@ -1,11 +1,13 @@
 import { Router } from 'express';
-import { createHouse, getHouses, getHouseById } from "../controllers/houseController";
+import { createHouse, getHouses, getHouseById, searchHouses } from "../controllers/houseController";
 import upload from '../middleware/upload';
 
 const router = Router();
 
 router.get("/houses", getHouses);
+router.get("/houses/search", searchHouses);
 router.get('/houses/:id', getHouseById);
+
 router.post('/houses', upload.array('images', 15), createHouse);
 
 export default router;
